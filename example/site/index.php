7859ebc5c768cd7f974002f2033b5ff6a14a14a1
murage <murage@labskenya.com>
2021-12-14 19:09:17 +0300

    docs: demo using the php docker image

---
+++ example/site/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "This is from php file that does nothing";
+echo "</br> </br> Click <a href='demo.txt'>here</a> to download a text file";
+echo " </br> </br>If you visit an invalid path, this content will also be printed out";
+
+echo " </br> </br> List of all available libraries available at <a href='site-info.php'>Site info</a>";
+echo "</br> </br> Good luck";
